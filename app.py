3e8aef5eea20574b01f1adfff197abcda61ef5ff
Krishna Kumar <92383929+krishna12q@users.noreply.github.com>
2026-06-04 14:54:37 +0530

    #2 Home Page Redirects Functional

--- app.py
+++ app.py
@@ -15,5 +15,14 @@ def createchatroom():
 def joinchatroom():
     return render_template("joinchat.html")
 
+@app.route("/signup", methods=['POST'])
+def createacc():
+    return render_template("register.html")
+
+@app.route("/login", methods=['POST'])
+def loginacc():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
